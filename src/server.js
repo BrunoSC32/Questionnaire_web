ae4bd3c26334672f6115e3fa5ce7d431f6743e55
Bruno Salcedo <bruno.salcedo.ca@gmail.com>
2025-11-23 02:52:55 -0400

    feat: add PostgreSQL integration and update dependencies

--- src/server.js
+++ src/server.js
@@ -1,11 +1,25 @@
-// src/server.js
-import app from "./app.js";
-import dotenv from "dotenv";
 
+import dotenv from "dotenv";
 dotenv.config();
+
+import app from "./app.js";
+import pool from "./config/db.js"; 
 
 const PORT = process.env.PORT || 3000;
 
+const testDbConnection = async () => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    console.log("Conected to PostgresSQL:", result.rows[0].now);
+  } catch (error) {
+    console.error("Error to conect PostgreSQL:", error?.message || error);
+    if (error?.stack) {
+      console.error(error.stack);
+    }
+  }
+};
+
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT}`);
+  testDbConnection(); 
 });
